c6398f4560a3a328b3e0d361d4e01be3aa2b1bb8
yangzhuokun <yangzhuokun@foxmail.com>
2024-01-29 23:49:43 +0800

    init

---
+++ v2ray_cli.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cur_dir=$(pwd)
+cfg_path=/home/zkyang/.config/v2ray
+subscription_path=${cfg_path}/subscriptions
+
+cd ${subscription_path}
+
+# Get list of .json files in specified directory
+files=(*.json)
+
+select file in "${files[@]}"; do
+    if [[ $REPLY == "0" ]]; then
+        echo 'Bye!' >&2
+        exit
+    elif [[ -z $file ]]; then
+        echo 'Invalid choice, try again' >&2
+    else
+        break
+    fi
+done
+
+echo $file
+
+ln -sf ${subscription_path}/"$file" ${cfg_path}/config.json
+
+systemctl --user restart v2ray.service
+
+cd $cur_dir
+
+echo "SOCKS5 PORT = 20170"
+echo "HTTP   PORT = 20171"
+
+systemctl --user status v2ray.service
